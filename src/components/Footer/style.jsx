dca16ec468375ad8d25ca8757fe1b1a1b0eecdc4
Claire Delaleau <claire.delaleau@gmail.com>
2022-02-07 23:35:49 +0100

    Delete pictos, add text

--- src/components/Footer/style.jsx
+++ src/components/Footer/style.jsx
@@ -1,23 +1,13 @@
 import styled from 'styled-components';
 
 const SHeader = styled.header`
-  height: auto;
+  height: 5vh;
+  margin-bottom: 2vh;
   width: auto;
   display: flex;
   justify-content: space-around;
-  align-items: center;
-  position: relative;
   color: black;
   font-family: Georgia, 'Times New Roman', Times, serif;
-
-  img {
-    border: solid 2px black;
-    border-radius: 20px;
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-    width: auto;
-    height: 200px;
-  }
 `;
 
 export default SHeader;
